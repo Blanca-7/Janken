e2fd8e35dc5d11bbafd62fca261794e64ac29b96
Arthyr M <arthyrxd@hotmail.de>
2021-08-12 18:05:41 +0200

    Add computerPlay() function
    This function randomly generates
    an integer & assigns one to a string

--- janken.js
+++ janken.js
@@ -1,1 +1,11 @@
-
+// CPU randomly generates & prints rock, paper, scissors
+function computerPlay() {
+  var generation = Math.floor(Math.random() * 3);
+  if (generation == 0) {
+    return "rock";
+  } else if (generation == 1) {
+    return "scissors";
+  } else {
+    return "paper";
+  }
+}
