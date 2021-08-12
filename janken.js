c6ee8b9551571ab9fdde1828f492ef893e8fc549
Arthyr M <arthyrxd@hotmail.de>
2021-08-12 18:17:32 +0200

    Add 1 round of Janken
    Added a single round of Janken. The user
    aka playerSelection has a fixed value of
    rock for now

--- janken.js
+++ janken.js
@@ -9,3 +9,21 @@ function computerPlay() {
     return "paper";
   }
 }
+
+// 1 round of 'static' Janken, user has fixed value of "rock"
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+  console.log(`User: ${playerSelection}`);
+  console.log(`Cpu:  ${computerSelection}`);
+  if (playerSelection == computerSelection) {
+    return "\n It's a tie!";
+  } else if (playerSelection && computerSelection == "scissors") {
+    return "You won! Rock beats scissors";
+  } else {
+    return "You lost! Paper beats rock";
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
