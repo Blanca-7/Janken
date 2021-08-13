89c5a96ee6c322a7aecfb656feecb0922eda4544
Arthyr M <arthyrxd@hotmail.de>
2021-08-13 09:18:38 +0200

    Add case-insensitive user-input
    Converts user-input to all lowercase
    Then converts only firstLetter toUppCase
    Lastly, adds rest of string back with .slice()

--- janken.js
+++ janken.js
@@ -2,33 +2,27 @@
 function computerPlay() {
   var generation = Math.floor(Math.random() * 3);
   if (generation == 0) {
-    return "rock";
+    return "Rock";
   } else if (generation == 1) {
-    return "scissors";
+    return "Scissors";
   } else {
-    return "paper";
+    return "Paper";
   }
 }
 
-// 1 round of 'static' Janken, user has fixed value of "rock"
-var playerSelection;
+// // 1 round of 'static' Janken, user has fixed value of "rock"
+var playerSelection = prompt(
+  "Type either: Rock, Paper and Scissors"
+).toLowerCase(); // Prompt for user input and convert it to lowerCase
 const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt(
-    "Type either: Rock, Paper and Scissors"
-  ).toLowerCase(); // Prompt for user input and converts it to lower-case
-  var firstLetter = playerSelection[0].toUpperCase(); // Upper-case first letter
-  var finalString = firstLetter + playerSelection.slice(1); // Slicing back rest of string
-  playerSelection = finalString; // Assining new casing of string back to original variable
+  var firstLetter = playerSelection[0].toUpperCase(); // toUpperCase first letter only
+  playerSelection = firstLetter + playerSelection.slice(1); // Slicing back rest of string
   console.log(`User: ${playerSelection}`);
   console.log(`Cpu:  ${computerSelection}`);
   if (playerSelection == computerSelection) {
     return "\n It's a tie!";
-  } else if (playerSelection && computerSelection == "scissors") {
-    return "You won! Rock beats scissors";
-  } else {
-    return "You lost! Paper beats rock";
   }
 }
 
