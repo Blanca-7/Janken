c2f79284b8d38a9ff508d039aa119db8002f7f29
Arthyr M <arthyrxd@hotmail.de>
2021-08-14 09:41:30 +0200

    Add game() function to play 5 rounds
    The game() function wraps around the
    playRound() function. It calls it 5x

--- janken.js
+++ janken.js
@@ -22,59 +22,63 @@ let computerSelection;
 let playerScore = 0;
 let cpuScore = 0;
 
-// // 5 rounds of Janken
-function playRound(playerSelection, computerSelection) {
-  computerSelection = computerPlay();
-  playerSelection = prompt(
-    "Type either: Rock, Paper and Scissors" // Prompt for user input
-  ).toLowerCase(); // and convert it to lowerCase
+//5 rounds of Janken
+function game() {
+  console.log(playRound(playerSelection, computerSelection));
+  //single round of Janken
+  function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = prompt(
+      "Type either: Rock, Paper and Scissors" // Prompt for user input
+    ).toLowerCase(); // and convert it to lowerCase
 
-  var firstLetter = playerSelection[0].toUpperCase(); // toUpperCase first letter only
-  playerSelection = firstLetter + playerSelection.slice(1); // Slicing back rest of string
-  console.log(`User: ${playerSelection}`);
-  console.log(`Cpu:  ${computerSelection}`);
+    var firstLetter = playerSelection[0].toUpperCase(); // toUpperCase first letter only
+    playerSelection = firstLetter + playerSelection.slice(1); // Slicing back rest of string
+    console.log(`User: ${playerSelection}`);
+    console.log(`Cpu:  ${computerSelection}`);
 
-  if (playerScore >= 2) {
-    console.log(`Game finished! You won!`);
-    return;
-  }
-  if (cpuScore >= 2) {
-    console.log(`Game finished! You lost!`);
-    return;
-  }
+    if (playerScore >= 2) {
+      console.log(`Game finished! You won!`);
+      return;
+    }
+    if (cpuScore >= 2) {
+      console.log(`Game finished! You lost!`);
+      return;
+    }
 
-  if (playerSelection == computerSelection) {
-    return `\n It's a tie!
+    if (playerSelection == computerSelection) {
+      return `\n It's a tie!
     it stands: ${playerScore} to ${cpuScore}`;
-  }
-  if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    return `You won! ${playerSelection} beats ${computerSelection}
+    }
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+      return `You won! ${playerSelection} beats ${computerSelection}
     Score:  ${++playerScore} to ${cpuScore}`;
-  }
-  if (playerSelection == "Rock" && computerSelection == "Paper") {
-    return `You lost! ${computerSelection} beats ${playerSelection}
+    }
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+      return `You lost! ${computerSelection} beats ${playerSelection}
     Score:  ${playerScore} to ${++cpuScore}`;
-  }
-  if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    return `You won! ${playerSelection} beats ${computerSelection}
+    }
+    if (playerSelection == "Scissors" && computerSelection == "Paper") {
+      return `You won! ${playerSelection} beats ${computerSelection}
     Score:  ${++playerScore} to ${cpuScore}`;
-  }
-  if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    return `You lost! ${computerSelection} beats ${playerSelection}
+    }
+    if (playerSelection == "Scissors" && computerSelection == "Rock") {
+      return `You lost! ${computerSelection} beats ${playerSelection}
     Score:  ${playerScore} to ${++cpuScore}`;
-  }
-  if (playerSelection == "Paper" && computerSelection == "Rock") {
-    return `You won! ${playerSelection} beats ${computerSelection}
+    }
+    if (playerSelection == "Paper" && computerSelection == "Rock") {
+      return `You won! ${playerSelection} beats ${computerSelection}
     Score:  ${++playerScore} to ${cpuScore}`;
-  }
-  if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    return `You lost! ${computerSelection} beats ${playerSelection}
+    }
+    if (playerSelection == "Paper" && computerSelection == "Scissors") {
+      return `You lost! ${computerSelection} beats ${playerSelection}
     Score:  ${playerScore} to ${++cpuScore}`;
+    }
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
+game();
+game();
+game();
+game();
+game();
