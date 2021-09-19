6f039735c3dbe5b9ad924035c1442d3c349ffb04
Arthyr M <arthyrxd@hotmail.de>
2021-09-19 09:12:04 +0200

    Add simple UI
    Refactored code. Used DOM to create and make buttons work.

--- janken.js
+++ janken.js
@@ -18,25 +18,25 @@ var cpuScore = 0;
 var scoreValuePlayer = 0;
 var scoreValueCpu = 0;
 
-game();
+//game();
 
 // game() function asks and converts user-input and invokes playRound()
-function game() {
-  computerSelection = computerPlay();
-  playerSelection = prompt(
-    "Type either: Rock, Paper or Scissors" // Prompt for user input
-  ).toLowerCase();
-  var firstLetter = playerSelection[0].toUpperCase(); // toUpperCase first letter only
-  playerSelection = firstLetter + playerSelection.slice(1); // Slicing back rest of string
-  console.log(playRound(playerSelection, computerSelection)); //calls playRound() with assigned parameters
-}
+// function game() {
+//   computerSelection = computerPlay();
+//   playerSelection = prompt(
+//     "Type either: Rock, Paper or Scissors" // Prompt for user input
+//   ).toLowerCase();
+//   var firstLetter = playerSelection[0].toUpperCase(); // toUpperCase first letter only
+//   playerSelection = firstLetter + playerSelection.slice(1); // Slicing back rest of string
+//   console.log(playRound(playerSelection, computerSelection)); //calls playRound() with assigned parameters
+// }
 
-// scoreCounter
+// // scoreCounter  THE LOGIC THAT PLAYS UNTIL EITHER PLAYR OR CPU REACHES 3 POINTS
 function gameTracker() {
-  if (scoreValuePlayer >= 4 || scoreValueCpu >= 4) {
-    console.log("Game Finished!");
-  } else {
-    game();
+  if (scoreValuePlayer >= 4) {
+    gameWon.textContent = "Game Finished! You won the game !";
+  } else if (scoreValueCpu >= 4) {
+    gameLost.textContent = "Game Finished! You lost the game !";
   }
 }
 
@@ -48,23 +48,60 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     scoreValuePlayer = playerScore;
     scoreValueCpu = cpuScore;
-    console.log(`\n It's a tie! Score: ${playerScore} to ${cpuScore}`);
+    result.textContent = `It's a tie! ${computerSelection} to ${playerSelection}`;
+    scoreCount.textContent = `Score: ${playerScore} to ${cpuScore}`;
   } else if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Rock")
   ) {
     scoreValuePlayer += ++playerScore;
-    console.log(`You won! ${playerSelection} beats ${computerSelection}
-    Score:  ${playerScore} to ${cpuScore}`);
+    result.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+    scoreCount.textContent = `Score:  ${playerScore} to ${cpuScore}`;
   } else if (
     (playerSelection == "Rock" && computerSelection == "Paper") ||
     (playerSelection == "Scissors" && computerSelection == "Rock") ||
     (playerSelection == "Paper" && computerSelection == "Scissors")
   ) {
     scoreValueCpu += ++cpuScore;
-    console.log(`You lost! ${computerSelection} beats ${playerSelection}
-    Score:  ${playerScore} to ${cpuScore}`);
+    result.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
+    scoreCount.textContent = `Score:  ${playerScore} to ${cpuScore}`;
   }
   gameTracker();
 }
+
+const rockBtn = document.querySelector("#rock");
+
+rockBtn.addEventListener("click", (rock) => {
+  playRound("Rock", computerPlay());
+});
+
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", (paper) => {
+  playRound("Paper", computerPlay());
+});
+
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", (scissors) => {
+  playRound("Scissors", computerPlay());
+});
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+const result = document.createElement("p");
+container.appendChild(result);
+
+const scoreCount = document.createElement("p");
+container.appendChild(scoreCount);
+
+const gameWon = document.createElement("h4");
+gameWon.style.color = "green";
+container.appendChild(gameWon);
+
+const gameLost = document.createElement("h4");
+gameLost.style.color = "red";
+container.appendChild(gameLost);
+
+// const lineBreak = document.createElement("p");
+// document.body.appendChild(lineBreak);
